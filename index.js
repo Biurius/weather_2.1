463fbee6337de2fc0fb598d90299f12502770913
Biurius <114857082+Biurius@users.noreply.github.com>
2022-12-27 21:05:48 +0000

    supposed add API on Forecast

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ function showcurrentTemp(response) {
   let currentDescription = response.data.weather[0].description;
   let descriptionElement = document.querySelector("#weather-description");
   descriptionElement.innerHTML = `${currentDescription}`;
+
+  getForecast(response.data.coord);
 }
 
 navigator.geolocation.getCurrentPosition(showPosition);
@@ -80,6 +82,14 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
 
   return `${day}, ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
 }
 
 function selectCity(response) {
@@ -118,6 +128,8 @@ function selectCity(response) {
   let currentDescription = response.data.weather[0].description;
   let descriptionElement = document.querySelector("#weather-description");
   descriptionElement.innerHTML = `${currentDescription}`;
+
+  getForecast(response.data.coord);
 }
 
 function showgeoTemp(event) {
@@ -133,22 +145,35 @@ function showPosition(position) {
   axios.get(`${apiUrl}`).then(showcurrentTemp);
 }
 
-function displayForecast() {
-  let forecastElement = document.querySelector("#weather-forecast");
+function getForecast(coordinates) {
+  let apiKey = "cce42a02472a5d3bb424243005811a2d3";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["thu", "fri", "sat","sun", "mon"];
-  days.forEach(function(day){
-    forecastHTML =
-      forecastHTML +
-      `<div class="col align-self-center">
-          <h6>${day}</h6>
-          <img src="images/red/sun.png" alt="sunny" width="50" height="50" />
-          <p class="maxemin">26° | 22°</p>
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col align-self-center">
+          <h6>${formatDay(forecastDay.dt)}</h6>
+          <img src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"  width="50" height="50" />
+          <p class="maxemin">${Math.round(
+            forecastDay.temp.max
+          )}° | ${Math.round(forecastDay.temp.min)}°</p>
         </div>`;
-  })
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
+    }
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+  });
 }
 
 function changeCel(event) {
